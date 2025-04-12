b02ecb67e0a3b32c1ce01a7c309b3c8bde9a1278
fsegovia <felipe.segovia.rod@gmail.com>
2025-04-12 09:07:15 -0400

    Se agrega tanstack query y zustand para la gestion de servicios y de estados de la aplicación

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,17 +1,17 @@
-import {Navigate, Route, Routes} from "react-router";
-import AuthPage from "./pages/AuthPage.tsx";
-import {PATHS} from "./const/paths.ts";
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router'
+import AuthPage from './pages/AuthPage.tsx'
+import { PATHS } from './const/paths.ts'
 
 const AppRoutes = () => {
-    return (
-        <Routes>
-            <Route index path={PATHS.AUTH} element={<AuthPage />} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path={PATHS.AUTH} element={<AuthPage />} />
 
-            <Route path="*" element={<Navigate to={PATHS.AUTH} />} />
-
-        </Routes>
-
-    )
+        <Route path="*" element={<Navigate to={PATHS.AUTH} />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default AppRoutes
